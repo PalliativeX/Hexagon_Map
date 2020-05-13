a60d5ed6b6d4b2e2be0b9bafeb27dd562975c319
PalliativeX <vvovvan009@yandex.ru>
2020-05-14 00:51:54 +0300

    Managing maps

--- Assets/Scripts/HexMapEditor.cs
+++ Assets/Scripts/HexMapEditor.cs
@@ -121,33 +121,6 @@ public class HexMapEditor : MonoBehaviour
 		hexGrid.ShowUI(visible);
 	}
 
-	public void Save()
-	{
-		string path = Path.Combine(Application.persistentDataPath, "test.map");
-		using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
-		{
-			writer.Write(0);
-			hexGrid.Save(writer);
-		}
-	}
-
-	public void Load()
-	{
-		string path = Path.Combine(Application.persistentDataPath, "test.map");
-		using (BinaryReader reader= new BinaryReader(File.OpenRead(path)))
-		{
-			int header = reader.ReadInt32();
-			if (header == 0)
-			{
-				hexGrid.Load(reader);
-			}
-			else
-			{
-				Debug.LogWarning("Unknown map formar " + header);
-			}
-		}
-	}
-
 	void Update()
 	{
 		if (
